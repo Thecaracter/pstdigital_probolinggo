4bc7aae23a442554b82d76c6dbdadf10fb06e391
Thecaracter <92218072+Thecaracter@users.noreply.github.com>
2023-12-14 20:48:19 +0700

    login

---
+++ resources/views/layout/vendor-scripts.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ URL::asset('login/assets/libs/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('login/assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ URL::asset('login/assets/libs/node-waves/node-waves.min.js') }}"></script>
+<script src="{{ URL::asset('login/assets/libs/feather-icons/feather-icons.min.js') }}"></script>
+<script src="{{ URL::asset('login/assets/js/pages/plugins/lord-icon-2.1.0.min.js') }}"></script>
+<script src="{{ URL::asset('login/assets/js/plugins.min.js') }}"></script>
+@yield('script')
+@yield('script-bottom')
