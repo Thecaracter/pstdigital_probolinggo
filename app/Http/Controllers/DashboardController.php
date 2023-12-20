307ce6c73acbd392a4a9ad996b9c20969966ee92
Thecaracter <92218072+Thecaracter@users.noreply.github.com>
2023-12-20 23:22:43 +0700

    .

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\KonsultasiVirtual;
+use App\Models\Catalog;
 use Illuminate\Http\Request;
+use App\Models\KonsultasiVirtual;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $konsultasiCount = KonsultasiVirtual::count();
+        $bukuCount = Catalog::count();
 
-        return view('admin.dashboard', compact('konsultasiCount'));
+        return view('admin.dashboard', compact('konsultasiCount', 'bukuCount'));
     }
 }
